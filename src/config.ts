1ff6e9f34caf47819f1d4ef10c86a7d2cb391f6a
satoshi.watanabe@heart.co.jp <satoshi.watanabe@heart.co.jp>
2023-05-24 17:34:09 +0900

    linter

--- src/config.ts
+++ src/config.ts
@@ -1,153 +1,153 @@
-import type { MarkdownInstance } from 'astro'
-import type { CollectionEntry } from 'astro:content'
+import type { MarkdownInstance } from "astro";
+import type { CollectionEntry } from "astro:content";
 
-export type Frontmatter = CollectionEntry<'blog'>['data']
+export type Frontmatter = CollectionEntry<"blog">["data"];
 
 export interface TagType {
-  tag: string
-  count: number
-  pages: CollectionEntry<'blog'>[]
+  tag: string;
+  count: number;
+  pages: CollectionEntry<"blog">[];
 }
 
 export const SiteMetadata = {
-  title: 'Hello Astro',
-  description: 'An Astro starter for corporate/marketing/blog websites.',
+  title: "Hello Astro",
+  description: "An Astro starter for corporate/marketing/blog websites.",
   author: {
-    name: 'Chris Tham',
-    twitter: '@chris1tham',
-    url: 'https://christham.net',
-    email: 'chris@christham.net',
-    summary: 'Outrageous actualiser.',
+    name: "Chris Tham",
+    twitter: "@chris1tham",
+    url: "https://christham.net",
+    email: "chris@christham.net",
+    summary: "Outrageous actualiser.",
   },
   org: {
-    name: 'Hello Tham',
-    twitter: '@hellothamcom',
-    url: 'https://hellotham.com',
-    email: 'info@hellotham.com',
+    name: "Hello Tham",
+    twitter: "@hellothamcom",
+    url: "https://hellotham.com",
+    email: "info@hellotham.com",
     summary:
-      'Hello Tham is a boutique management consulting firm. We specialise in Business and IT strategies, operating models, strategic roadmaps, enterprise architecture, analytics and business process design.',
+      "Hello Tham is a boutique management consulting firm. We specialise in Business and IT strategies, operating models, strategic roadmaps, enterprise architecture, analytics and business process design.",
   },
-  location: 'Rivendell, Middle Earth',
+  location: "Rivendell, Middle Earth",
   latlng: [-33.86785, 151.20732] as [number, number],
-  repository: 'https://github.com/hellotham/hello-astro',
+  repository: "https://github.com/hellotham/hello-astro",
   social: [
     {
-      name: 'Email',
-      link: 'mailto:info@hellothamcom',
-      icon: 'envelope',
+      name: "Email",
+      link: "mailto:info@hellothamcom",
+      icon: "envelope",
     },
     {
-      name: 'Phone',
-      link: '555-5555',
-      icon: 'telephone',
+      name: "Phone",
+      link: "555-5555",
+      icon: "telephone",
     },
     {
-      name: 'LinkedIn',
-      link: 'https://www.linkedin.com/hellothamcom',
-      icon: 'linkedin',
+      name: "LinkedIn",
+      link: "https://www.linkedin.com/hellothamcom",
+      icon: "linkedin",
     },
     {
-      name: 'Facebook',
-      link: 'https://www.facebook.com/hellothamcom',
-      icon: 'facebook',
+      name: "Facebook",
+      link: "https://www.facebook.com/hellothamcom",
+      icon: "facebook",
     },
     {
-      name: 'Instagram',
-      link: 'https://www.instagram.com/hellothamcom',
-      icon: 'instagram',
+      name: "Instagram",
+      link: "https://www.instagram.com/hellothamcom",
+      icon: "instagram",
     },
     {
-      name: 'Twitter',
-      link: 'https://twitter.com/hellothamcom',
-      icon: 'twitter',
+      name: "Twitter",
+      link: "https://twitter.com/hellothamcom",
+      icon: "twitter",
     },
     {
-      name: 'Github',
-      link: 'https://github.com/hellothamcom',
-      icon: 'github',
+      name: "Github",
+      link: "https://github.com/hellothamcom",
+      icon: "github",
     },
   ],
   buildTime: new Date(),
-}
+};
 
-export const Logo = '../images/svg/astro/logomark-light.svg'
-export const LogoImage = '../images/astro/full-logo-light.png'
-export const FeaturedSVG = '../images/svg/undraw/undraw_design_inspiration.svg'
-export const DefaultSVG = '../images/svg/undraw/undraw_my_feed.svg'
-export const DefaultImage = '../images/undraw/undraw_my_feed.png'
+export const Logo = "../images/svg/astro/logomark-light.svg";
+export const LogoImage = "../images/astro/full-logo-light.png";
+export const FeaturedSVG = "../images/svg/undraw/undraw_design_inspiration.svg";
+export const DefaultSVG = "../images/svg/undraw/undraw_my_feed.svg";
+export const DefaultImage = "../images/undraw/undraw_my_feed.png";
 
 export const NavigationLinks = [
-  { name: 'はじめに', href: '' },
-  { name: '業務情報', href: 'blog' },
-  { name: '製品サポート', href: 'doc/introduction' },
-]
+  { name: "はじめに", href: "" },
+  { name: "業務情報", href: "blog" },
+  { name: "製品サポート", href: "doc/introduction" },
+];
 
 export const CategoryDetail = [
   {
-    category: 'instructions',
-    coverSVG: '../images/svg/undraw/undraw_instruction_manual.svg',
-    socialImage: '../images/undraw/undraw_instruction_manual.png',
-    description: 'Guidelines on using this starter.'
+    category: "instructions",
+    coverSVG: "../images/svg/undraw/undraw_instruction_manual.svg",
+    socialImage: "../images/undraw/undraw_instruction_manual.png",
+    description: "Guidelines on using this starter.",
   },
   {
-    category: 'information',
-    coverSVG: '../images/svg/undraw/undraw_instant_information.svg',
-    socialImage: '../images/undraw/undraw_instant_information.png',
-    description: 'Information articles.'
+    category: "information",
+    coverSVG: "../images/svg/undraw/undraw_instant_information.svg",
+    socialImage: "../images/undraw/undraw_instant_information.png",
+    description: "Information articles.",
   },
-]
+];
 
 export function categoryDetail(category: string | undefined) {
-  const details = CategoryDetail.filter(cat => cat.category == category)
+  const details = CategoryDetail.filter((cat) => cat.category == category);
 
   if (details.length == 1) {
-    return details[0]
+    return details[0];
   }
   return {
-    category: 'General',
-    coverSVG: '../images/svg/undraw/undraw_instant_information.svg',
-    socialImage: '../images/undraw/undraw_instant_information.png',
-    description: 'Category ' + category,
-  }
+    category: "General",
+    coverSVG: "../images/svg/undraw/undraw_instant_information.svg",
+    socialImage: "../images/undraw/undraw_instant_information.png",
+    description: "Category " + category,
+  };
 }
 export const AuthorDetail = [
   {
-    name: 'Chris Tham',
-    description: 'Hello World',
-    contact: 'chris@christham.net',
-    image: '../images/authors/Chris Tham.jpg'
-  }
-]
+    name: "Chris Tham",
+    description: "Hello World",
+    contact: "chris@christham.net",
+    image: "../images/authors/Chris Tham.jpg",
+  },
+];
 
 export const DefaultAuthor = {
-  name: 'Hello Astro',
-  image: '../images/authors/default.png',
-  contact: 'info@hellotham.com',
-  description: '著者'
-}
+  name: "Hello Astro",
+  image: "../images/authors/default.png",
+  contact: "info@hellotham.com",
+  description: "著者",
+};
 
 export function authorDetail(author: string | undefined) {
-  const details = AuthorDetail.filter(person => person.name == author)
+  const details = AuthorDetail.filter((person) => person.name == author);
 
   if (details.length == 1) {
-    return details[0]
+    return details[0];
   }
-  return DefaultAuthor
+  return DefaultAuthor;
 }
 
-export const PAGE_SIZE = 6
+export const PAGE_SIZE = 6;
 
-export const GITHUB_EDIT_URL = `https://github.com/hellotham/hello-astro`
+export const GITHUB_EDIT_URL = `https://github.com/hellotham/hello-astro`;
 
-export const COMMUNITY_INVITE_URL = `https://astro.build/chat`
+export const COMMUNITY_INVITE_URL = `https://astro.build/chat`;
 
-export type Sidebar = Record<string, { text: string; link: string }[]>
+export type Sidebar = Record<string, { text: string; link: string }[]>;
 
 export const SIDEBAR: Sidebar = {
-  '製品サポート': [
-    { text: '用語の定義', link: 'doc/introduction' },
-    { text: '機能一覧', link: 'doc/page-2' },
-    { text: '操作方法', link: 'doc/page-3' },
+  製品サポート: [
+    { text: "用語の定義", link: "doc/introduction" },
+    { text: "機能一覧", link: "doc/page-2" },
+    { text: "操作方法", link: "doc/page-3" },
   ],
-  '初期設定': [{ text: 'ブラウザ設定', link: 'doc/page-4' }],
-}
+  初期設定: [{ text: "ブラウザ設定", link: "doc/page-4" }],
+};
--- src/content/doc/page-4.md
+++ src/content/doc/page-4.md
@@ -3,20 +3,21 @@ section: 初期設定
 title: ブラウザ設定
 description: Lorem ipsum dolor sit amet - 4
 ---
+
 #### フォント設定
 
 帳票を適切な文字サイズで表示するために以下の手順に従って設定を行います。使用するブラウザによって設定箇所が異なります。
 
-##### Chromeの場合
+##### Chrome の場合
 
-- アドレスバーに ``chrome://settings/fonts``を貼り付ける。
+- アドレスバーに `chrome://settings/fonts`を貼り付ける。
 - 最小フォントサイズを下限値にする。
 
 ![image](https://user-images.githubusercontent.com/22786124/212241939-9c30713f-d713-4743-8836-b855eb87793c.png)
 
-##### Edgeの場合
+##### Edge の場合
 
-- アドレスバーに ``edge://settings/fonts``を貼り付ける。
+- アドレスバーに `edge://settings/fonts`を貼り付ける。
 - 最小フォントサイズを下限値にする。
 
 ![image](https://user-images.githubusercontent.com/22786124/212242049-685571db-b8fb-41bd-be8c-f4bc6c81a8fb.png)
