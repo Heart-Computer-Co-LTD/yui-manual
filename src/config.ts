0f71e0bfce631ed42e7343e21905c9c5e1e18b90
satoshi.watanabe@heart.co.jp <satoshi.watanabe@heart.co.jp>
2023-12-28 03:00:44 +0900

    トップの画像を変更

--- src/config.ts
+++ src/config.ts
@@ -36,7 +36,7 @@ export const SiteMetadata = {
 
 export const Logo = '../images/svg/astro/logomark-light.png'
 export const LogoImage = '../images/astro/full-logo-light.png'
-export const FeaturedSVG = '../images/svg/undraw/undraw_design_inspiration.svg'
+export const FeaturedSVG = '../images/svg/undraw/topimage.png'
 export const DefaultSVG = '../images/svg/undraw/undraw_my_feed.svg'
 export const DefaultImage = '../images/undraw/undraw_my_feed.png'
 
